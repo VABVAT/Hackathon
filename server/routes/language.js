8a74ffbc82dd3e2851678fd3b05041cc11ff1c76
Vaibhav <sidanavaibhav@gmail.com>
2024-09-28 19:19:39 +0530

    e

--- server/routes/language.js
+++ server/routes/language.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-router.use(cors());
+// router.use(cors());
 const translateText = async (text) => {
     try {
         const res = await translate(text, { to: 'en' });
