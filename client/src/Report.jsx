f2cff9021cb832cedbb629636c965545fe0ddab6
VABVAT <136883802+VABVAT@users.noreply.github.com>
2024-10-02 02:43:22 +0530

    Update Report.jsx

--- client/src/Report.jsx
+++ client/src/Report.jsx
@@ -48,7 +48,7 @@ function Report() {
   const getSOl = async () => {
     try {
 
-      const response = await fetch("http://flaskback.online:8080/classify", {
+      const response = await fetch("https://flaskback.online/classify", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
