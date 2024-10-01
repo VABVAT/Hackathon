c8a295b0188f9c68301bf9b86bee86fea8e93158
VABVAT <136883802+VABVAT@users.noreply.github.com>
2024-10-02 00:10:48 +0530

    Update Report.jsx

--- client/src/Report.jsx
+++ client/src/Report.jsx
@@ -48,7 +48,7 @@ function Report() {
   const getSOl = async () => {
     try {
 
-      const response = await fetch("/api/proxy.js", {
+      const response = await fetch("/api/proxy", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
